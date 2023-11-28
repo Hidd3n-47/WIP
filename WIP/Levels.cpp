312bc00f919fbda1a6c6f0d52b34875238a5ff03
John <jl9crt@bolton.ac.uk>
2023-11-28 21:00:38 +0100

    LevelmapParserVer1 I'm terrible at naming... Currently missing: -values for width and height of each square -wall and floor textures -actual script for creating walls and floors since I lack the debug values... but it's basically just calling the function with the right arguments -Any other map objects which currently haven't been decided on

---
+++ WIP/Levels.cpp
@@ -0,0 +1,124 @@
+#include "pch.h"
+#include "Levels.h"
+#include <sstream>
+#include <Engine/ECS/BoxCollider.h>
+#include <Engine/ECS/SpriteRenderer.h>
+
+Levels::Levels()
+{
+}
+
+Levels::~Levels()
+{
+}
+
+float Levels::getMaxLength(std::string string)
+{
+	//expects the firstline of every map to be 1x maxlength
+	float maxLength = stoi(split(string, 'x').at(1));//should technically be right regardless of preparsed or not, as long as it is the first line
+	return maxLength;
+}
+
+float Levels::getRows(std::string string)
+{
+	//expects original unparsed mapstring
+	float rows = split(string, '\n').size();
+	return rows;
+}
+
+void Levels::createWall(float x, float y)
+{
+	jci::Entity* newWall = jci::SceneManager::Instance()->GetCurrentScene()->CreateEmptyEntity();//create empty entity
+	newWall->GetComponent<jci::Transform>()->SetPosition({ x,  y });
+	newWall->AddComponent<jci::BoxCollider>()->SetBodyType(jci::BodyType::Kinematic);
+	newWall->AddComponent<jci::SpriteRenderer>()->SetColor({ 0.4f, 0.4f, 0.4f });//REPLACE THIS LINE WITH WALL TEXTURE
+
+
+}
+
+void Levels::createFloor(float x, float y)
+{
+	jci::Entity* newFloor = jci::SceneManager::Instance()->GetCurrentScene()->CreateEmptyEntity();//create empty entity
+	newFloor->GetComponent<jci::Transform>()->SetPosition({ x,  y });
+	newFloor->AddComponent<jci::SpriteRenderer>()->SetColor({ 0.4f, 0.4f, 0.4f });//REPLACE THIS LINE WITH WALL TEXTURE
+
+}
+
+std::vector<std::string> Levels::split(const std::string& string, const char splitter)//function that splits input string into a vector;
+{
+	std::vector<std::string> result;//to be outputted result
+	std::stringstream splitString(string);//convert string to stringstream
+	std::string push;//pushed result
+	while (std::getline(splitString, push, splitter))
+	{
+		result.push_back(push);//pushback result
+	}
+	return result;// with the debugLevelString will look like [0]1x30, [1]\n, [2]1, [3]2x28...
+}
+
+void Levels::LoadDEBUGLevel()
+{
+	//debug room 30x30
+	//take in room string
+	std::string debugLevelString = "1x30 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1 2x28 1 \n 1x30";
+	LoadLevel(debugLevelString);
+}
+
+void Levels::LoadLevel(std::string fileString)
+{
+	std::vector<std::string>parsedString = split(fileString, ' ');//split via spaces first
+	float length = getMaxLength(parsedString.at(0));//in this context the input is 1x30
+	float rows = getRows(fileString);//this and the above are used to do math to center level on camera via coordinate maths
+	float currentX = (0 - ((length * width) / 2));//this is for centering wall length around camera // spawning walls from left to right assuming 0 is center of screen
+	float currentY = (0 + ((rows * height) / 2));//this is for centering wall length around camera // spawning walls from top to bottom assuming 0 is center of screen
+	for (auto i : parsedString)
+	{
+		//take parsedString and start creating the level squares;
+		if (split(i, 'x').size() > 1)//if multiply step
+		{
+			std::vector<std::string>smallString = split(i, 'x');//split via any multipliers
+			int times = stoi(smallString.at(1));//int times is the amount of times to create this type of square;
+			for (int i = 0; i < times; i++)
+			{
+				switch (stoi(smallString.at(0)))
+				{
+				case 1://wall
+					//script to create wall at locations
+					break;
+				case 2://floor
+					//script to create floor at locations
+					break;
+				default:
+					jci::Log::DebugLog("John's Error: Square's key is invalid!");
+					break;
+				}
+				//iterate currentX
+				currentX += width;
+			}
+		}
+		else//if no multiply step, do accordingly
+		{//switch case was not used here where newline was applicable
+			if (i == "\n")//if new line...
+			{
+				currentY -= height;//step down next layer
+				currentX = (0 - ((length * width) / 2));//this is for centering wall length around camera
+			}
+			else if (i == "1")
+			{
+				//script to create wall at locations
+
+			}
+			else if (i == "2")
+			{
+				//script to create floors at locations
+			}
+			else
+			{
+				jci::Log::DebugLog("John's Error: Square's key is invalid!");
+			}
+			currentX += width;//iterate length of singular square
+		}
+	}
+
+
+}
---
+++ WIP/Levels.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <vector>
+#include <Engine/Scene/SceneManager.h>
+#include <Engine/ECS/Entity.h>
+
+class Levels
+{
+private:
+	std::vector<jci::Entity*> LevelSquare;
+
+
+public:
+	Levels();
+	~Levels();
+
+	float getMaxLength(std::string string);//get max length of string (used to determine x value)
+	float getRows(std::string string);//get amount of rows in string (used to determine y value) //this and the above are used to do math to center level on camera via coordinate maths
+	void createWall(float x, float y);
+	void createFloor(float x, float y);
+	std::vector<std::string> split(const std::string& string, const char splitter);//string parser;
+	void LoadDEBUGLevel();
+	void LoadLevel(std::string fileString);
+
+	/*//////////////////////////////////////FORMAT/////////////////////////////////////
+	* Levels are loaded by splitting fileString multiple ways;
+	* Easiest put is example: "1x5 \n 1 2x3 1 \n 1x5"
+	* Translates into: Wallx5 newline Wall Floorx3 Wall newline Wallx5
+	* See Key at bottom of file
+	* 
+	* Load level takes the string input, first splitting it by spaces and then placing each item into a vector array of strings
+	* Vector array of strings is then parsed again, any item with an 'x' inside is considered a multiplier and will run the first value's equivalent key object that number of times
+	* A.k.A "1x5" becomes 5 walls
+	* 
+	* Any singular number without a multiplier is treated as just an individual space
+	* 
+	* And any \n or in txt files: newline(s) are considered starting a new row
+	* 
+	* ///////////////////////////IMPORTANT///////////////////////////////
+	* The first line of the map is used to calculate the x and y values of the ENTIRE map's squares
+	* It starts the first square at half the max length of the first map and ALWAYS ASSUMES that the first line is ENTIRELY WALLS
+	* If this is a problem do send John a message or work it out if you can make sense of what I just wrote, hardly a clue myself
+	* 
+	* ALSO, breakpoints are unfinished lines that must be finished
+	*/
+
+
+	//square sizes used for calculating x and ys...
+	const float width;
+	const float height;//these can be replaced with getters if validated elsewhere....
+
+	//key:              Currently just a ghost integer since the switch cases and if statements are a touch annoying and neither actually use a pure int... But yes if it really boils down to it we can do a bunch of casting to make this actually used...
+	const int wall = 1;
+	const int floor = 2;
+
+};
+
