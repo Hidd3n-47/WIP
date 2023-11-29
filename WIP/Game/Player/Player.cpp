9363d9063f15722b3d121c86f8f3a52dda9ca749
Hidd3n-47 <122316898+Hidd3n-47@users.noreply.github.com>
2023-11-29 17:52:05 +0000

    Input Manager handle mouse movement, Fps shown in window title
    Input now handles mouse input.
     - Get mouse position
     - Get mouse button down
    
    Window title includes fps.

--- WIP/Game/Player/Player.cpp
+++ WIP/Game/Player/Player.cpp
@@ -37,23 +37,32 @@ void Player::FireGun()
 
 void Player::Update()
 {
+	vec2 direction = vec2(0.0f);
+
+	const float SPEED = 0.05f;
+
 	if (jci::InputManager::Instance()->IsKeyPressed(jci::Keycode_w))
 	{
-		playChar->GetComponent<jci::Transform>()->AddToPosition({ 0.0f, 0.05f });
+		direction += vec2(0.0f, SPEED);
 	}
 	if (jci::InputManager::Instance()->IsKeyPressed(jci::Keycode_s))
 	{
-		playChar->GetComponent<jci::Transform>()->AddToPosition({ 0.0f, -0.05f });
+		direction += vec2(0.0f, -SPEED);
 	}
 	if (jci::InputManager::Instance()->IsKeyPressed(jci::Keycode_a))
 	{
-		playChar->GetComponent<jci::Transform>()->AddToPosition({ -0.05f, 0.0f });
+		direction += vec2(-SPEED, 0.0f);
 	}
 	if (jci::InputManager::Instance()->IsKeyPressed(jci::Keycode_d))
 	{
-		playChar->GetComponent<jci::Transform>()->AddToPosition({ 0.05f, 0.0f });
+		direction += vec2(SPEED, 0.0f);
 	}
-	if (jci::InputManager::Instance()->IsKeyPressed(jci::Keycode_e))
+
+	glm::normalize(direction);
+
+	playChar->GetComponent<jci::Transform>()->AddToPosition(direction);
+
+	if (jci::InputManager::Instance()->IsKeyPressed(jci::Button_Left))
 	{
 		FireGun();
 	}
