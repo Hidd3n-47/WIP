ea03ebbaac5e44e6a171b3b23c3dc51d5c02f9c0
IZZAPPER1 <isaackw15465@gmail.com>
2023-12-19 15:09:54 +0000

    Barricaded door sprite, added variables to 'Gun'

--- WIP/Game/Gun/Gun.h
+++ WIP/Game/Gun/Gun.h
@@ -12,5 +12,9 @@ public:
 
 private:
 	int m_fireRate;
-
+	int m_reloadSpeed;
+	int m_magSize;
+	int m_inaccuracy; // +/- The angle the bullet will leave the gun at. 0 is going straight ahead
+	bool m_isBounce;
+	bool m_isPenetrate;
 };
